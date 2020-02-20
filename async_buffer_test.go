510c5d441199d6919a3fc724dd8246970c236262
Brian Martens <brian.martens@corvium.com>
2020-02-20 14:41:46 -0500

    updated test logs and added randomized load test
    Signed-off-by: Brian Martens <brian.martens@corvium.com>

--- async_buffer_test.go
+++ async_buffer_test.go
@@ -1,15 +1,13 @@
 package async_buffer
 
 import (
-	"fmt"
+	"math/rand"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestAsyncBuffer1wr1rd(t *testing.T) {
-	defer func() {
-		fmt.Println(recover())
-	}()
 	buf, exit := NewBuffer()
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -25,12 +23,10 @@ func TestAsyncBuffer1wr1rd(t *testing.T) {
 		readTestData(buf)
 	}()
 	wg.Wait()
+	t.Log("successfully tested 1 reader and 1 writer")
 }
 
 func TestAsyncBuffer2wr1rd(t *testing.T) {
-	defer func() {
-		fmt.Println(recover())
-	}()
 	buf, exit := NewBuffer()
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -57,12 +53,10 @@ func TestAsyncBuffer2wr1rd(t *testing.T) {
 		readTestData(buf)
 	}()
 	wg.Wait()
+	t.Log("successfully tested 1 reader and 2 writers")
 }
 
 func TestAsyncBuffer1wr2rd(t *testing.T) {
-	defer func() {
-		fmt.Println(recover())
-	}()
 	buf, exit := NewBuffer()
 	var wg sync.WaitGroup
 	wg.Add(3)
@@ -83,12 +77,10 @@ func TestAsyncBuffer1wr2rd(t *testing.T) {
 		readTestData(buf)
 	}()
 	wg.Wait()
+	t.Log("successfully tested 2 readers and 1 writer")
 }
 
 func TestAsyncBuffer2wr2rd(t *testing.T) {
-	defer func() {
-		fmt.Println(recover())
-	}()
 	buf, exit := NewBuffer()
 	var wg sync.WaitGroup
 	wg.Add(3)
@@ -120,21 +112,54 @@ func TestAsyncBuffer2wr2rd(t *testing.T) {
 		readTestData(buf)
 	}()
 	wg.Wait()
+	t.Log("successfully tested 2 readers and 2 writers")
+}
+
+func TestRandomizedLoad(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		rand.Seed(time.Now().UnixNano())
+		buf, exit := NewBuffer()
+		var wg sync.WaitGroup
+		nRand, gRand := rand.Intn(50)+1, rand.Intn(50)+1
+		// g readers
+		for g := 0; g < gRand; g++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				readTestData(buf)
+			}()
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			var wrwg sync.WaitGroup
+			for n := 0; n < nRand; n++ {
+				wrwg.Add(1)
+				go func() {
+					defer wrwg.Done()
+					putTestData(buf)
+				}()
+			}
+			wrwg.Wait()
+			exit()
+		}()
+		wg.Wait()
+		t.Logf("successfully tested %d writers and %d readers\n", nRand, gRand)
+	}
 }
 
 func putTestData(buffer *AsyncBuffer) {
 	for i := 0; i < 100; i++ {
 		buffer.Put(i)
-		fmt.Printf("put: %v\n", i)
 	}
 }
 
 func readTestData(buffer *AsyncBuffer) {
 	for {
 		v := buffer.Get()
-		if v == nil && buffer.IsClosed() {
+		if v == nil {
 			break
 		}
-		fmt.Printf("get: %v\n", v)
 	}
 }
